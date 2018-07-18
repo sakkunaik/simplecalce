de5c84322b16168a6daf9579734db530a40823c1
sakkunaik <sakkunaik000@gmail.com>
2018-07-18 11:51:03 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/simplecalce/Calculator.java
@@ -0,0 +1,30 @@
+package com.example.android.simplecalce;
+
+public class Calculator
+{
+    public enum Operator {ADD, SUB, DIV, MUL}
+    public double add(double firstOperand, double secondOperand)
+    {
+
+        return firstOperand + secondOperand;
+
+    }
+
+    public double sub(double firstOperand, double secondOperand) {
+
+        return firstOperand - secondOperand;
+
+    }
+    public double div(double firstOperand, double secondOperand) {
+
+        return firstOperand / secondOperand;
+
+    }
+
+    public double mul(double firstOperand, double secondOperand) {
+
+        return firstOperand * secondOperand;
+
+    }
+
+}
